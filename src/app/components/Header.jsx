0c6966e58921b73c0103ef3a321284acf1b54c13
shahzadisakeena <shahzadisakeena6@gmail.com>
2025-02-06 02:16:40 +0500

    header&hero

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,40 +1,93 @@
+'use client'
+import { Facebook, Instagram, Linkedin, Menu, Twitter, X, Youtube } from 'lucide-react';
 import Link from 'next/link';
-import React from 'react'
-
+import React from 'react';
+import { useState } from 'react';
 const Header = () => {
-    const navLinks = [
-        { name: "Home", path: "/" },
-        { name: "About", path: "/" },
-        { name: "Services", path: "/" },
-        { name: "Pricing", path: "/" },
-        { name: "Careers", path: "/" },
-      ];
-  return (
-    <nav className=' flex items-center w-[75%] mx-auto mt-5 px-5 bg-Frost-white h-24 rounded-xl'>
-        <div className='flex items-center justify-center  space-x-24 mx-auto'>
-            <div>
-                <h2 className='text-4xl font-bold text-Purple font-Bebas'>WebAura</h2>
-            </div>
-            <div className=' flex items-center justify-between text-Deep-blue space-x-12'>
-                {navLinks.map((link)=>{
-                    return(
-                        <Link
-                        key={link.name}
-                        href={link.path}
-                        >
-                        {link.name}
-                        </Link>
-                    )
-                })}
-            </div>
-<div>
-    <button className='bg-Purple px-8 py-3 tracking-wider text-white rounded-lg'>
-        Get in Touch
-    </button>
-</div>
-        </div>
-    </nav>
-  )
-}
+  const [isOpen , setIsOpen] = useState(false);
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/" },
+    { name: "Services", path: "/" },
+    { name: "Pricing", path: "/" },
+    { name: "Careers", path: "/" },
+  ];
 
-export default Header
+  return (
+    <>
+    <nav className='flex items-center justify-between lg:w-[80%] w-[90%] mx-auto mt-5 lg:px-5 px-3 bg-Frost-white lg:h-24 h-16 rounded-xl'>
+      {/* Left Section - Logo */}
+      <div>
+        <h2 className='lg:text-4xl text-3xl font-bold text-Purple font-Bebas'>WebAura</h2>
+      </div>
+
+      {/* Center Section - Navigation */}
+      <div className='lg:flex hidden items-center text-black space-x-16'>
+        {navLinks.map((link) => (
+          <Link key={link.name} href={link.path}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+      {/* mobile menu */}
+      <div className='lg:hidden flex '>
+        <button onClick={()=> setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <X className='text-Purple' strokeWidth={3} size={30}/>
+          ):(
+            <Menu className='text-Purple' strokeWidth={3} size={30} />
+          )}
+        </button>
+
+      </div>
+{/* <div className='lg:hidden flex'>
+  <Menu className='text-Purple' 
+  strokeWidth={3}
+  size={30}
+  />
+</div> */}
+      {/* Right Section - Button */}
+      <div className="lg:block hidden">
+        <button className='bg-Purple px-8 py-3 tracking-wider text-white rounded-lg'>
+          Get in Touch
+        </button>
+      </div>
+    </nav>
+    <div className={`absolute top-24 left-1/2 -translate-x-1/2 w-[90%] h-[85vh] rounded-xl bg-Frost-white transition-all duration-500 ${
+      isOpen ? "opacity-100 visible scale-100" : "opacity-0 invisible scale-95"
+    } lg:hidden`}>
+      <div className='text-Purple flex flex-col items-center space-y-9 text-lg   pt-20 '>
+      {navLinks.map((link)=>(
+        <Link key={link.name} href={link.path} onClick={()=> setIsOpen(false)}>{link.name}</Link>
+      ))}
+      </div>
+     
+      <button className='bg-Purple px-8 py-3 flex flex-col items-center justify-center mt-12 mx-auto w-[70vw] tracking-wider rounded-lg text-white'
+      onClick={()=> setIsOpen(false)}
+      >
+        Get in Touch
+      </button>
+      <div className='flex space-x-5 text-Purple mt-10 items-center justify-center pb-4'>
+<div className='bg-Cyan rounded-full p-3 text-Purple'>
+        <Instagram />
+       </div>
+               <div className='bg-Cyan  rounded-full p-3 text-Purple'>
+               <Facebook/>
+               </div>
+               <div className='bg-Cyan  rounded-full p-3 text-Purple'>
+               <Linkedin/>
+               </div>
+               <div className='bg-Cyan  rounded-full p-3 text-Purple'>
+               <Twitter/>
+               </div>
+               <div className='bg-Cyan  rounded-full p-3 text-Purple'>
+               <Youtube/>
+               </div>
+
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Header;
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,25 +1,49 @@
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from 'lucide-react'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div className=' flex flex-col mx-auto items-center justify-center mt-28  w-[80%]'>
-      <h1 className='font-Bebas text-Frost-white text-8xl  text-center'>
-      &ldquo; Your Vision&#44; Our Code – Custom <br /><span className='text-Cyan'>Websites That Perform</span> &rdquo;
+    <div className=' flex mx-auto'>
+      <div className='flex mx-auto items-center justify-center  flex-col lg:w-[80%] mt-28'>
+      <h1 className='font-Bebas text-Frost-white lg:text-8xl text-4xl  text-center mx-3'>
+       Your Vision&#44; Our Code – Custom <br /><span className='text-Cyan'>Websites That Perform</span>
 
       </h1>
-      <p className='text-gray-100 text-xl mx-32 mt-3 text-center   '>
+      <p className='text-gray-100 text-xl lg:mx-32 mx-14 mt-5 text-center   '>
       We craft custom websites that bring your vision to life with sleek design&#44; seamless functionality&#44;  
       and optimized performance to drive real results.
       </p>
-      <div className='flex flex-row justify-center items-center mx-auto space-x-8 mt-16'>
-        <button className='bg-Cyan px-10 py-4 tracking-wider text-lg font-semibold text-Purple rounded-lg'
+      <div className='flex lg:flex-row flex-col justify-center items-center mx-auto lg:space-x-8 mt-16'>
+        <button className='bg-Cyan px-10 py-4 tracking-wider text-lg w-[70vw] lg:w-[20vw] font-semibold text-Purple rounded-lg'
         >
           Book a Call
         </button>
-        <button className='border-Cyan border-2 px-10 py-4 tracking-wider font-semibold text-Cyan text-lg rounded-lg'>
+        <button className='border-Cyan border-2 px-10 py-4 tracking-wider lg:w-[20vw] w-[70vw]  lg:mt-0 mt-5 font-semibold text-Cyan text-lg rounded-lg'>
           Learn More
         </button>
       </div>
+     
+      </div>
+      <div className='lg:flex hidden flex-col mt-28 bg-Purple p-4 rounded-full space-y-10 mr-6'>
+        <div className='bg-Bright hover:bg-Cyan rounded-full p-3 text-Purple hover:scale-125 transition-transform duration-300'>
+        <Instagram/>
+        </div>
+        <div className='bg-Bright hover:bg-Cyan  rounded-full p-3 text-Purple hover:scale-125 transition-transform duration-300'>
+        <Facebook/>
+        </div>
+        <div className='bg-Bright hover:bg-Cyan  rounded-full p-3 text-Purple hover:scale-125 transition-transform duration-300'>
+        <Linkedin/>
+        </div>
+        <div className='bg-Bright hover:bg-Cyan  rounded-full p-3 text-Purple hover:scale-125 transition-transform duration-300'>
+        <Twitter/>
+        </div>
+        <div className='bg-Bright hover:bg-Cyan  rounded-full p-3 text-Purple hover:scale-125 transition-transform duration-300'>
+        <Youtube/>
+        </div>
+
+      </div>
+
+
 </div>
   )
 }
